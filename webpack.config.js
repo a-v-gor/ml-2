f759e2a61e2e565d2e0a9d68b4184d4d68bf3a2b
Alexey Gorbenko <a.v.gor@mail.ru>
2021-07-28 13:29:35 +0300

    Добавлена автогенерация новых страниц

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
+const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const PATHS = {
+  src: path.join(__dirname, './src'),
+  dist: path.join(__dirname, './dist'),
+  assets: 'assets/'
+};
+const PAGES_DIR = `${PATHS.src}/${PATHS.assets}pages/`;
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: PATHS.dist,
     filename: '[name].js',
   },
   module: {
     rules: [{ test: /\.pug$/, loader: 'pug-loader', options: { pretty: true } }],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.pug',
-    })
+    ...PAGES.map(page => new HtmlWebpackPlugin ({
+      template: `${PAGES_DIR}/${page}`,
+      filename: `./${page.replace(/\.pug/,'.html')}`
+    }))
 ],
 };
